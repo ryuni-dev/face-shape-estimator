c976f4b9136fc5a75f2df563073bb06a038068ea
ryun-dev@gmail.com <ryuni.dev@gmail.com>
2022-07-28 16:46:45 +0900

    loss 수정

--- model/loss.py
+++ model/loss.py
@@ -4,4 +4,4 @@ def nll_loss(output, target):
     return F.nll_loss(output, target)
 
 def cross_entropy_loss(output, target):
-    return F.CrossEntropyLoss(output, target)
+    return F.cross_entropy(output, target)
